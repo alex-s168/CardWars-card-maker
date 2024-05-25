091268b2fb371cae01ee24d48cf3c11363370943
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-05-25 16:14:25 +0200

    stop complaining ktor

--- server/src/main/kotlin/cwcardmaker/plugins/Routing.kt
+++ server/src/main/kotlin/cwcardmaker/plugins/Routing.kt
@@ -6,25 +6,30 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.date.*
+import kotlinx.coroutines.*
 import java.io.File
 
 fun Application.configureRouting() {
     routing {
         get("/file") {
-            call.request.queryParameters["name"]?.let {
-                val fn = it.replace(File.separatorChar, '_')
-                if (fn.startsWith("file-")) {
-                    val file = File(fn)
-                    call.respondFile(file)
+            withContext(Dispatchers.IO) {
+                call.request.queryParameters["name"]?.let {
+                    val fn = it.replace(File.separatorChar, '_')
+                    if (fn.startsWith("file-")) {
+                        val file = File(fn)
+                        call.respondFile(file)
+                    }
                 }
             }
         }
         post("/save") {
-            val bytes = call.receiveStream().readAllBytes()
-            val name = "file-${getTimeMillis()}.zip"
-            val file = File(name)
-            file.writeBytes(bytes)
-            call.respondText("http://207.180.202.42:7070/file?name=$name")
+            withContext(Dispatchers.IO) {
+                val bytes = call.receiveStream().readAllBytes()
+                val name = "file-${getTimeMillis()}.zip"
+                val file = File(name)
+                file.writeBytes(bytes)
+                call.respondText("http://207.180.202.42:7070/file?name=$name")
+            }
         }
         staticFiles("/",
             File("../build/dist/js/productionExecutable/")
